2f183a913df581072a9e0dd25109816a15cfead9
syiaril <uyearil1@gmail.com>
2025-11-20 06:00:11 +0700

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,50 @@
+<?php exit; // DO NOT DELETE
+// Config OJS membaca database dari .env
+
+function env($key, $default = null) {
+    return getenv($key) ?: $default;
+}
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+[general]
+
+app_key =
+installed = On
+base_url = "<?php echo env('BASE_URL', 'https://ojs.ubig.web.id'); ?>"
+strict = Off
+session_cookie_name = OJSSID
+session_lifetime = 30
+session_samesite = Lax
+time_zone = "UTC"
+date_format_short = "Y-m-d"
+date_format_long = "F j, Y"
+datetime_format_short = "Y-m-d h:i A"
+datetime_format_long = "F j, Y - h:i A"
+time_format = "h:i A"
+allow_url_fopen = Off
+restful_urls = Off
+allowed_hosts = ''
+trust_x_forwarded_for = Off
+show_upgrade_warning = On
+enable_minified = Off
+enable_beacon = On
+sitewide_privacy_statement = Off
+user_validation_period = 28
+sandbox = Off
+
+[database]
+
+driver = "<?php echo env('DB_DRIVER', 'mysqli'); ?>"
+host = "<?php echo env('DB_HOST', 'localhost'); ?>"
+username = "<?php echo env('DB_USERNAME', 'ojs_user'); ?>"
+password = "<?php echo env('DB_PASSWORD', 'ojs_pass'); ?>"
+name = "<?php echo env('DB_DATABASE', 'ojs_db'); ?>"
+port = <?php echo env('DB_PORT', 3306); ?>
+ssl = false
+debug = Off
+
+[files]
+files_dir = "<?php echo env('OJS_FILES_DIR', '/var/www/ojs/files'); ?>"
+public_files_dir = "<?php echo env('OJS_PUBLIC_DIR', '/var/www/ojs/public'); ?>"
+public_user_dir_size = 5000
+umask = 0022
